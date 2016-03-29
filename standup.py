ed3fb7ea00c421220d4db3fecaf74fc22882e09a
Robert Bedard <rbedard@plex.com>
2016-03-29 12:11:19 -0400

    Refactored code for future enhancements

--- standup.py
+++ standup.py
@@ -1,13 +1,18 @@
 import ctypes
 import time
 
+def showPopup(message):
+    ctypes.windll.user32.MessageBoxW(0, message, "Do this", 0)
+
 loops = 0
 
 print("Press ctrl-C to stop the program early (runs about 8ish hours)")
 
 while loops < 8:
-    ctypes.windll.user32.MessageBoxW(0, "You should stand up now.", "Do this", 0)
-    time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
+    showPopup("You should stand up now.")
+    print("You should be standing.")
+    time.sleep(5)  # Delay for 30 minutes (1800 seconds)
     ctypes.windll.user32.MessageBoxW(0, "Sit down now.", "Do this", 0)
-    time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
+    print("You should sit be sitting.")
+    time.sleep(5)  # Delay for 30 minutes (1800 seconds)
     loops += 1
