ed0723becd5dd7ad9fe4eac9f541fe4ea2fb3f29
Robert Bedard <rbedard@plex.com>
2016-03-29 09:11:04 -0400

    Added message that displays in command line

--- standup.py
+++ standup.py
@@ -3,6 +3,8 @@ import time
 
 loops = 0
 
+print("Press ctrl-C to stop the program early (runs about 8ish hours)")
+
 while loops < 8:
     ctypes.windll.user32.MessageBoxW(0, "You should stand up now.", "Do this", 0)
     time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
