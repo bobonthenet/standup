170aa44d16eacfa029c1d58d29bc3146c1b045be
Robert Bedard <rbedard@plex.com>
2016-03-29 12:31:41 -0400

    Fixed errors

--- standup.py
+++ standup.py
@@ -3,12 +3,12 @@ import time
 import sys
 
 def showPopup(message):
-    if(sys.version_info >= 3)
+    if(sys.version_info >= (3,0)):
         ctypes.windll.user32.MessageBoxW(0, message, "Do this", 0)
-        print(message)
-    else
+    else:
         ctypes.windll.user32.MessageBoxA(0, message, "Do this", 0)
-        print message
+    print(message)
+
 
 loops = 0
 
@@ -16,7 +16,7 @@ print("Press ctrl-C to stop the program early (runs about 8ish hours)")
 
 while loops < 8:
     showPopup("You should stand up now.")
-    time.sleep(5)  # Delay for 30 minutes (1800 seconds)
+    time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
     showPopup("Sit down now.")
-    time.sleep(5)  # Delay for 30 minutes (1800 seconds)
+    time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
     loops += 1
