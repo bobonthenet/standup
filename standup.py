111ee5abeaae9f4a65c9a5103d86dda60b654c82
Robert Bedard <rbedard@plex.com>
2016-03-29 13:17:01 -0400

    Will Now Except Execution Parameters

--- standup.py
+++ standup.py
@@ -9,14 +9,26 @@ def showPopup(message):
         ctypes.windll.user32.MessageBoxA(0, message, "Do this", 0)
     print(message)
 
+def intTryParse(value):
+    try:
+        return int(value)
+    except ValueError:
+        return 1800
 
 loops = 0
+interval = [1800,1800] #defaults to 30 minutes.
+i = 0
+
+for timeFrame in sys.argv[1:]:
+    interval[i] = intTryParse(timeFrame)
+    i += 1
 
 print("Press ctrl-C to stop the program early (runs about 8ish hours)")
 
 while loops < 8:
     showPopup("You should stand up now.")
-    time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
+    time.sleep(interval[0])
     showPopup("Sit down now.")
-    time.sleep(1800)  # Delay for 30 minutes (1800 seconds)
+    time.sleep(interval[1])
+
     loops += 1
